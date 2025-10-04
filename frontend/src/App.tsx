9d472c05c3eba67e3a64db39faad8954d0570065
Damir <damir.bro09@gmail.com>
2025-10-04 14:56:51 +0300

    Добавление роутинга в app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchDone from "@/pages/SearchDone";
 
 function App() {
-  return <SearchDone />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Главная страница</div>} />
+        <Route path="/search-done" element={<SearchDone />} />
+        {/* Добавьте другие маршруты здесь */}
+        <Route path="*" element={<div>Страница не найдена</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
