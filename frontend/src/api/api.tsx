a150014e9334a59dad06aa99da0f804b18cf01ec
Damir <damir.bro09@gmail.com>
2025-10-04 17:05:37 +0300

    Упрощение функции apiFetch и добавление типов для улучшения обработки ответов API

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -1,26 +1,16 @@
-import { CourseOpen, CourseStructure } from "@/types/courseMenu";
-import { LessonsStructure } from "@/types/lesson";
-import { UserProfile } from "@/types/users";
-import { QuestionsStructure } from "./types/question";
-
 export const IP = "https://skill-force.ru";
-export const PORT = "80";
 
-export interface Course {
-  id: number;
-  price: number;
-  purchases_amount: number;
-  creator_id: number;
-  time_to_pass: number;
-  title: string;
-  description: string;
-  rating: number;
-  src_image: string;
-  tags: string[];
-  is_favorite: boolean;
-}
+export type FetchResponse<T> = {
+  data?: T;
+  error?: string;
+  status?: number;
+  ok: boolean;
+};
 
-export async function apiFetch(url: string, options = {}) {
+export async function apiFetch<T = any>(
+  url: string,
+  options = {}
+): Promise<FetchResponse<T>> {
   try {
     const response = await fetch(`${IP}/api${url}`, {
       credentials: "include",
@@ -29,14 +19,51 @@ export async function apiFetch(url: string, options = {}) {
 
     const data = await response.json();
     if (!response.ok) {
-      throw new Error(data.error || `Ошибка: ${response.status}`);
+      return {
+        ok: false,
+        error: data.error || `Ошибка: ${response.status}`,
+        status: response.status,
+      };
     }
 
-    return data;
-  } catch (error) {
+    return {
+      ok: true,
+      data,
+      status: response.status,
+    };
+  } catch (error: any) {
     console.error(`Ошибка запроса к ${url}:`, error);
-    return null;
+    return {
+      ok: false,
+      error: error.message || "Неизвестная ошибка",
+    };
   }
+}
+
+export async function apiFetchGET<T = any>(
+  url: string
+): Promise<FetchResponse<T>> {
+  return apiFetch<T>(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export async function apiFetchPOST<T = any>(
+  url: string,
+  body: any
+): Promise<FetchResponse<T>> {
+  const csrfToken = await fetchCSRFToken();
+  return apiFetch<T>(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRF-Token": csrfToken || "",
+    },
+    body: JSON.stringify(body),
+  });
 }
 
 export async function fetchCSRFToken() {
@@ -53,6 +80,7 @@ export async function fetchCSRFToken() {
 
   return csrfToken;
 }
+<<<<<<< HEAD
 
 export async function searchForm(keyword: string) {
   const data = await apiFetch(`/v1/search`, {
@@ -72,4 +100,3 @@ export async function ListOfSearching() {
     ? data
     : "Ошибка"
 }
-
