ee50f205dc9b91f917577e9072a3131a20d5cca1
nikolsafarova <nikolsafarova55@gmail.com>
2025-10-04 15:08:08 +0300

    nicole - first commit

---
+++ frontend/src/modules/Search/Search.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import search from "Public/static/icons/iconSearch.svg";
+import styles from "./Search.module.scss";
+
+
+export default function Search() {
+
+  // function debounce(func: (arg0: any) => void | PromiseLike<void>, ms: number | undefined) {
+  //   let timeout: string | number | NodeJS.Timeout | undefined;
+  //   return (...args: any) =>  {
+  //     clearTimeout(timeout);
+  //     timeout = setTimeout(() => func(args), ms);
+  //   };
+  // }
+  // const debouncedSetSearch = debounce((value: string) => {setSearch(value)}, 500)
+
+  return (
+    <div className={styles.searchForm}>
+      <img class={styles.searchSearchForm__icon} src={search}></img>
+      <input
+        type="text"
+        placeholder="Поиск"
+        class={styles.searchForm__input}
+      ></input>
+    </div>
+  );
+}
