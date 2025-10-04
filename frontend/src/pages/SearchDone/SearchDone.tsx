da0990effdc43beceafe087a41a4511f177e18ca
Damir <damir.bro09@gmail.com>
2025-10-04 14:36:51 +0300

    Добавление компонента SearchDone с соответствующими стилями и индексом

---
+++ frontend/src/pages/SearchDone/SearchDone.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./SearchDone.module.scss";
+
+export default function SearchDone() {
+  return (
+    <div className={styles.container}>
+      <h1>Search Done Page</h1>
+    </div>
+  );
+}
