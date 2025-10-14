b02a4d10452a450be295ab4be23bacd494cdb24a
Damir <damir.bro09@gmail.com>
2025-10-14 17:53:23 +0300

    feat: добавление генерации самоподписанных SSL сертификатов в Makefile и настройка HTTPS в vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  envDir: '..',
+  envDir: "..",
+  server: {
+    https:
+      fs.existsSync(path.resolve(__dirname, "ssl/key.pem")) &&
+      fs.existsSync(path.resolve(__dirname, "ssl/cert.pem"))
+        ? {
+            key: fs.readFileSync(path.resolve(__dirname, "ssl/key.pem")),
+            cert: fs.readFileSync(path.resolve(__dirname, "ssl/cert.pem")),
+          }
+        : {},
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src/"),
