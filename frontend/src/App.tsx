0f2c032e661c35d3219ff32a2a0495ab6acfb5d9
Damir <damir.bro09@gmail.com>
2025-10-14 07:35:18 +0300

    feat: добавление маршрута для страницы настроек интеграций

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AsideBar from "@/components/AsideBar";
 import SearchDone from "@/pages/SearchDone";
 import Document from "@/pages/Document/Document";
+import IntegrationSettings from "@/pages/IntegrationSettings";
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
           <Route path="/" element={<div>Главная страница</div>} />
           <Route path="/search-done" element={<SearchDone />} />
           <Route path="/document/:id" element={<Document />} />
+          <Route
+            path="/integration-settings"
+            element={<IntegrationSettings />}
+          />
           <Route path="*" element={<div>Страница не найдена</div>} />
         </Routes>
       </AsideBar>
