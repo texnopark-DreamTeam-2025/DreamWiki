ecccc993d4d14d348fe094c025b9c3fcf8106923
Damir <damir.bro09@gmail.com>
2025-10-14 18:05:54 +0300

    cc: импортирование стилей в main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import "./styles.scss";
 import "@gravity-ui/uikit/styles/fonts.css";
 import "@gravity-ui/uikit/styles/styles.css";
 import { StrictMode } from "react";
@@ -8,9 +9,13 @@ import { ThemeProvider } from "@gravity-ui/uikit";
 import { client } from "./client/client.gen.ts";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 
-client.setConfig(createConfig({
-  baseURL: import.meta.env.VITE_BASE_URL || "https://did.you.forget.to.add.vite.base.url.to.config/?",
-}));
+client.setConfig(
+  createConfig({
+    baseURL:
+      import.meta.env.VITE_BASE_URL ||
+      "https://did.you.forget.to.add.vite.base.url.to.config/?",
+  })
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
