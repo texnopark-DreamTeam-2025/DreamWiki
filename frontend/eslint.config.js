db99e77a37a5fae62bba6b2b491f0b2e74615d17
Damir <damir.bro09@gmail.com>
2025-10-15 18:56:31 +0300

    feat: добавление поддержки Prettier и настройка форматирования кода

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,10 +3,12 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist", "src"]),
+  globalIgnores(["dist", "src/client/**/*", "**/*.gen.ts"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
@@ -14,7 +16,14 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+      prettierConfig, // Отключает конфликтующие с Prettier правила
     ],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": "error", // Показывает ошибки форматирования Prettier как ESLint ошибки
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
